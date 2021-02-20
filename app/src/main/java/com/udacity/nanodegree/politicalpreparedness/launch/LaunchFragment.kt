e67f5fb4ad44e920c3c49cc44d1ea770923113f1
Deepak Dawade <deepakdawade.dd1@gmail.com>
2021-02-21 01:16:22 +0530

    navigation from launch to respective fragment

--- app/src/main/java/com/udacity/nanodegree/politicalpreparedness/launch/LaunchFragment.kt
+++ app/src/main/java/com/udacity/nanodegree/politicalpreparedness/launch/LaunchFragment.kt
@@ -1,34 +1,37 @@
 package com.udacity.nanodegree.politicalpreparedness.launch
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
 import com.udacity.nanodegree.politicalpreparedness.databinding.FragmentLaunchBinding
 
 class LaunchFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val binding = FragmentLaunchBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
-        binding.representativeButton.setOnClickListener { navToRepresentatives() }
-        binding.upcomingButton.setOnClickListener { navToElections() }
+        binding.launchFragmentActionFindMyRepresentatives.setOnClickListener { navToRepresentatives() }
+        binding.launchFragmentActionUpcomingElection.setOnClickListener { navToElections() }
 
         return binding.root
     }
 
     private fun navToElections() {
-        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToElectionsFragment())
+        this.findNavController()
+            .navigate(LaunchFragmentDirections.toElectionsFragment())
     }
 
     private fun navToRepresentatives() {
-        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToRepresentativeFragment())
+        this.findNavController()
+            .navigate(LaunchFragmentDirections.toRepresentativeFragment())
     }
 
 }
