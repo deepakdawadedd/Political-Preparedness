d8d8a425f0b0b1610a590bd4a507520a081f4aef
Deepak Dawade <deepakdawade.dd1@gmail.com>
2021-02-20 23:16:50 +0530

    starter code

---
+++ app/src/main/java/com/udacity/nanodegree/politicalpreparedness/database/ElectionDao.kt
@@ -0,0 +1,21 @@
+package com.udacity.nanodegree.politicalpreparedness.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface ElectionDao {
+
+    //TODO: Add insert query
+
+    //TODO: Add select all election query
+
+    //TODO: Add select single election query
+
+    //TODO: Add delete query
+
+    //TODO: Add clear query
+
+}
